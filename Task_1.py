d66d824b1928d41d2bbdd99f46116da6caec4925
aaditya-pdgupta <aadityaprasad.gupta@gmail.com>
2025-12-01 18:57:48 +0530

    Uploaded the Task_1 and Task_2

---
+++ Task_1.py
@@ -0,0 +1,34 @@
+# %%
+num1 = float(input("Enter the first number:"))
+
+# %%
+num2 = float(input("Enter the second number:"))
+
+# %%
+sum = num1 + num2
+
+# %%
+sub =  num1 - num2
+
+# %%
+mul =  num1 * num2
+
+# %%
+div =  num1 / num2
+
+# %%
+print("The addition of two number is", sum)
+
+# %%
+print("The subtraction of two number is", sub)
+
+# %%
+print("The multiplication of two number is", mul)
+
+# %%
+print("The division of two number is", div)
+
+# %%
+
+
+
