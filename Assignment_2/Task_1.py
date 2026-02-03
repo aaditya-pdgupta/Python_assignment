9abdc8e5f6811e2015cf3983fd2ffb72a4466939
Aaditya Prasad Gupta <aadityaprasad.gupta@gmail.com>
2026-02-03 22:09:13 +0530

    Updated the file

---
+++ Assignment_2/Task_1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+num = int(input("Enter a number:"))
+
+
+# In[ ]:
+
+
+if num % 2 == 0:
+    print(f"{num} is an even number")
+else:
+    print(f"{num} is a odd number")
+
+
+# In[5]:
+
+
+n = int(input("Enter a number:"))
+
+
+# In[10]:
+
+
+if n % 2 == 0:
+    print(f"{n} is an even number")
+else:
+    print(f"{n} is a odd number")
+
+
+# In[ ]:
+
+
+
+
---
+++ Assignment_2/Task_2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+s = 0
+
+
+# In[4]:
+
+
+for i in range (1, 51):
+    s +=  i 
+
+
+# In[6]:
+
+
+print(f"The sum of numbers from 1 to 50 is {s}")
+
+
+# In[ ]:
+
+
+
+
